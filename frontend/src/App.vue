8b4d30bc90864ae6b6be07695f98edbc719c7d30
Faez-B <faezbhatti@gmail.com>
2022-06-20 13:50:38 +0200

    Ajout d'une navbar dans l'app avec les liens de connexion et accueil Ajout de bootstrap et du module vue-router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,18 @@
 <script setup>
+import Navbar from './components/Navbar.vue';
 
 </script>
 
 <template>
   <header>
-    
+      <Navbar/>
   </header>
 
-  <main>
+  <main class="container">
     
   </main>
 </template>
 
 <style>
-
+  @import 'bootstrap/dist/css/bootstrap.min.css';
 </style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-2">
+
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+          
+          <li class="nav-item active"><RouterLink class="nav-link" to="/">Accueil</RouterLink></li>
+
+        </ul>
+
+      </div>
+
+      <ul class="navbar-nav mr-auto">
+        <li v-if="!user" class="nav-item">
+          <RouterLink class="nav-link btn btn-success" :to="{name:'connexion'}">Connexion</RouterLink>
+        </li>
+      </ul>
+    </nav>
+</template>

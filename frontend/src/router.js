1221ede9b5f008fd023fd28913f62639c52419ab
Faez-B <faezbhatti@gmail.com>
2022-06-20 14:01:05 +0200

    Ajout de la route pour l'accueil

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,9 +1,16 @@
 import { createWebHistory, createRouter } from "vue-router";
 import LoginVue from "@/components/Login.vue";
+import AccueilComponent from "@/components/Accueil.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {
+            path: "/",
+            name: "accueil",
+            component: AccueilComponent
+        },
+
         {
             path: "/connexion",
             name: "connexion",
